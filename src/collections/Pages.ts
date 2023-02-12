059d7f4480a3db67587fa59ee45f5a936f359366
wiktorkujawa <wiktorkujawa1993@gmail.com>
2023-02-12 23:14:55 +0100

    finished cms base

--- src/collections/Pages.ts
+++ src/collections/Pages.ts
@@ -1,34 +1,11 @@
-import payload from "payload";
 import { CollectionConfig } from "payload/types";
 import { WYSIWYG, Quote } from "../fields/components";
 
 const Pages: CollectionConfig = {
   slug: "pages",
-
   admin: {
-    useAsTitle: "Name",
+    useAsTitle: "name",
   },
-  endpoints: [
-    {
-      path: '/slug/:slug',
-      method: 'get',
-      handler: async (req, res, next) => {
-        const tracking = await payload.find({
-          collection:'pages',
-          where: {
-            slug: {
-              equals: req.params.slug
-            }
-          }
-        });
-        if (tracking) {
-          res.status(200).send({ data: tracking.docs });
-        } else {
-          res.status(404).send({ error: 'not found' });
-        }
-      }
-    }
-  ],
   access: {
     read: () => true,
   },
@@ -44,16 +21,12 @@ const Pages: CollectionConfig = {
     {
       name: "customComponents",
       type: "blocks",
-      // minRows: 0,
       maxRows: 20,
       blocks: [
-        // required
         WYSIWYG,
         Quote
       ],
-    },
-    // Email added by default
-    // Add more fields as needed
+    }
   ],
 };
 
